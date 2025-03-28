e0c7f74e33ad44c8d0dc59071edaf549cfec5518
Ssaimanikanta139-web <serusaimanikanta@gmail.com>
2025-03-28 10:14:03 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  client_id       = "a94fedc6-4a19-4d5f-adc2-b3d2ed89ef6c"
-  client_secret   = "Eol8Q~BeG~7iHwJc_xVvr4DVyJ1tvfBCP9Khydo6"
-  tenant_id       = "04dbe115-60d5-42fd-a414-2466c1bd253b"
-  subscription_id = "fafef1fd-8570-4dd8-b315-16af1fa631cf"
-}
